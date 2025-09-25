96ce5def0adebca267eb4265c0c7569ad4a9e5e1
Richi <ricardoclavijo0085@gmail.com>
2025-09-25 12:14:25 -0400

    fix base por domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from 'lovable-tagger';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-	base: '/massify/',
+	base: '/',
 	server: {
 		host: '::',
 		port: 8080,
